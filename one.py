58807898823febe5df0c9f54ef823dc28b38359b
yy11235813@163.com <jinzhiqiang@maxrocky.com>
2017-09-28 13:58:15 +0800

    update

--- one.py
+++ one.py
@@ -1,3 +1,48 @@
 #coding=utf-8
-name = input("请输入你的名字：")
-print ('hello,',name)
+#pyethon 联系
+#小明的成绩从去年的72分提升到了今年的85分，请计算小明成绩提升的百分点，并用字符串格式化显示出'xx.x%'，只保留小数点后1位：
+# s1 = 72
+# s2 = 85
+# r =(s2-s1)*100/s1
+# print('%.1f%%' % r)
+#请用索引取出下面list的指定元素：
+# L = [
+#     ['Apple','Google','Microsoft'],
+#     ['Java','Python','Ruby','PHP'],
+#     ['Adam','Bart','Lisa']
+# ]
+# print L[0][0]
+# print L[1][1]
+# print L[2][2]
+#小明身高1.75，体重80.5kg。请根据BMI公式（体重除以身高的平方）帮小明计算他的BMI指数，并根据BMI指数：
+#
+#     低于18.5：过轻
+#     18.5-25：正常
+#     25-28：过重
+#     28-32：肥胖
+#     高于32：严重肥胖
+#
+# 用if-elif判断并打印结果：
+
+# height = 1.75
+# weight = 80.5
+# bmi = weight / (height * height)
+# if bmi <= 18.5:
+#     print '过轻'
+# elif bmi >18.5 and bmi <= 25:
+#     print '正常'
+# elif bmi > 25 and bmi <= 28:
+#     print '过重'
+# elif bmi >28 and bmi <= 32:
+#     print '肥胖'
+# elif bmi > 32 :
+#     print '严重肥胖'
+#请利用循环依次对list中的每个名字打印出Hello, xxx!：
+# L = ['Bart','Lisa','Adam']
+# for l in L:
+#     print 'hello,',l
+#请利用Python内置的hex()函数把一个整数转换成十六进制表示的字符串：
+# n1 = 255
+# n2 = 1000
+# print hex(n1)
+# print hex(n2)
